657f61368a11b34d9f5fabe4b4167412991b92fd
huutam1991 <huutam1991@gmail.com>
2025-12-08 14:43:21 +0800

    Move server certificate generation to the non-production branch of run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-chmod 777 generate_server_certificate.sh
-./generate_server_certificate.sh
 
 # Detect port
 if [[ "$PROD" == "true" ]]; then
     PORT=443
 else
     PORT=8080
+    chmod 777 generate_server_certificate.sh
+    ./generate_server_certificate.sh
 fi
 
 # perf stat -e cycles,instructions,task-clock,context-switches ./order_book_server_cpp "$PORT" web_data
