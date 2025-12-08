0236c61a09d77fe108207102b1e86c980b95a24c
huutam1991 <huutam1991@gmail.com>
2025-12-08 14:25:24 +0800

    Remove commented-out lock_guard in LatencyTracker methods for clarity

--- core/utils/latency_tracker.h
+++ core/utils/latency_tracker.h
@@ -13,7 +13,7 @@ struct LatencyTracker
 
     inline void add_sample(double value)
     {
-        // std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard<std::mutex> lock(mtx);
         samples.push_back(value);
         if (samples.size() > max_samples)
         {
@@ -24,7 +24,7 @@ struct LatencyTracker
     // Get percentile (50, 90, 99)
     inline double percentile(double p)
     {
-        // std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard<std::mutex> lock(mtx);
         if (samples.empty())
         {
             return 0.0;
@@ -42,7 +42,7 @@ struct LatencyTracker
 
     inline void clear()
     {
-        // std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard<std::mutex> lock(mtx);
         samples.clear();
     }
 };
