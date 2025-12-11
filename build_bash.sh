3219d1b9894f143df2a590ac30312d55863c75a3
huutam1991 <huutam1991@gmail.com>
2025-12-11 18:53:43 +0800

    Refactor CMake command in build_bash.sh for improved readability and maintainability

--- build_bash.sh
+++ build_bash.sh
@@ -38,7 +38,14 @@ install_if_missing libspdlog-dev
 
 mkdir -p build
 cd build/
-cmake .. -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DENABLE_UNITY_BUILD=OFF -DBUILD_ONLY="core;sts;identitystore;s3;ec2"
+cmake .. \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER=g++ \
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -flto=8 -ffast-math -fno-plt" \
+  -DCMAKE_C_FLAGS="-O3 -march=native -mtune=native -flto=8 -ffast-math -fno-plt" \
+  -DENABLE_UNITY_BUILD=OFF \
+  -DBUILD_ONLY="core;sts;identitystore;s3;ec2"
 cmake --build . -j 6
 make -j
 chmod 777 order_book_server_cpp
